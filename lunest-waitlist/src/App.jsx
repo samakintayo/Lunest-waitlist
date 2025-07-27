3f7d19e9d81cd7ec34dac5038c65b5d0b307e1c1
Akinnayajo Akintayo <tayoakinnayajo@gmail.com>
2025-07-27 13:08:55 +0100

    more font fix

--- lunest-waitlist/src/App.jsx
+++ lunest-waitlist/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-
-
+import "./index.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Waitlistform from "./pages/Waitlistform";
