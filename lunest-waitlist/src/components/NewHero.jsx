8380d3f45ca1f2d255a2a89bdafe3aaf26257b98
Akinnayajo Akintayo <tayoakinnayajo@gmail.com>
2025-07-27 13:23:12 +0100

    font fix

--- lunest-waitlist/src/components/NewHero.jsx
+++ lunest-waitlist/src/components/NewHero.jsx
@@ -31,7 +31,7 @@ const NewHero = () => {
 
         {/* Main Text */}
         <div className="max-w-5xl text-center mb-6">
-          <h1 className="text-4xl md:text-6xl font-medium font-['aeonik']">
+          <h1 className="text-4xl md:text-6xl font-medium font-aeonik">
             The Future of{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-indigo-200">
               Smarter Home Rentals
@@ -50,7 +50,7 @@ const NewHero = () => {
           href="/waitlist"
           className="px-7 py-3.5 bg-blue-700 rounded-[30px] inline-flex justify-center items-center gap-3.5 mb-4"
         >
-          <span className="text-white text-xl font-medium font-['aeonik']">
+          <span className="text-white text-xl font-medium font-aeonik">
             Join the Waitlist
           </span>
           <div className="w-4 h-4 relative">
