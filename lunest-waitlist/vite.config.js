d52e33793be5f75e766aaac5dbb30eafae09ec3f
Akinnayajo Akintayo <tayoakinnayajo@gmail.com>
2025-10-11 21:48:44 +0100

    update

--- lunest-waitlist/vite.config.js
+++ lunest-waitlist/vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "dist",    // relative to frontend
+    emptyOutDir: true  // clears old builds
   },
-  base: "/", // 👈 This line ensures proper routing
+  base: "./"           // makes JS/CSS relative
 });
